86ed805bf57b8f44b25ebbad14f74527b4b2b451
Jenniferrmuniz <Jennifer.r.muniz@gmail.com>
2019-11-13 12:26:38 -0500

    changed blue

--- otherWay.js
+++ otherWay.js
@@ -15,7 +15,8 @@ window.onload = function () {
 
     getSquares();
 
-    function getSquares(){
+    // Creates squares array
+    function getSquares() {
         let counter = 1;
         for (let i = 20; i <= 400; i += 20) {
             for (let j = 20; j <= 400; j += 20) {
@@ -32,27 +33,22 @@ window.onload = function () {
 
     // Fills grid with squares
     function fillGrid() {
-
-
-        for (let i = 20; i <= 400; i += 20) {
-            for (let j = 20; j <= 400; j += 20) {
-                if (endpt) {
-                    if (getBlueSquares(i, j)) {
-
-                        ctx.fillStyle = "blue";
-                    }
-                    else {
-                        ctx.fillStyle = "gray";
-                    }
+        for (let i = 1; i < squares.length; i++) {
+            if (endpt) {
+                if (getBlueSquares(i)) {
+                    ctx.fillStyle = "blue";
                 }
                 else {
                     ctx.fillStyle = "gray";
                 }
-                ctx.fillRect(i, j, 10, 10);
             }
+            else {
+                ctx.fillStyle = "gray";
+            }
+            ctx.fillRect(squares[i].x, squares[i].y, 10, 10);
         }
         if (endpt) {
-            getRedCircles();
+            createRedCircles();
         }
     }
 
@@ -63,10 +59,10 @@ window.onload = function () {
     function createBlueCircle() {
         updateCanvas();
         if (endpt) {
-            radius = Math.hypot(center.x - endpt.x, center.y - endpt.y);
+            radius = getDistance(center.x, center.y, endpt.x, endpt.y);
         }
         else {
-            radius = Math.hypot(center.x - movingMouse.x, center.y - movingMouse.y);
+            radius = getDistance(center.x, center.y, movingMouse.x, movingMouse.y);
         }
 
         ctx.beginPath();
@@ -84,8 +80,8 @@ window.onload = function () {
         return {
             x: center.x + (Math.cos(angle) * radius),
             y: center.y + (Math.sin(angle) * radius),
-            width: 10,
-            height: 10
+            width: 1,
+            height: 1
         }
     }
 
@@ -104,37 +100,16 @@ window.onload = function () {
 
 
 
+
+
     // Checks if square is close enough to the circle to be colored blue
-    function getBlueSquares(xSquare, ySquare) {
-
+    function getBlueSquares(index) {
         let point;
-
-        for (let k = 0; k < 360; k++) {
-            point = getPoint(k);
-
-            if (checkAllCollisions(point)) {
-                console.log('here');
+        for (let k = 0; k < 360; k+=5) {
+            point = getPoint(k*(Math.PI/180));
+            if (checkCollision(point, squares[index])) {
                 return true;
             }
-
-            // point.x += 5;
-            // if (checkAllCollisions(point)) {
-            //     console.log('here');
-            //     return true;
-            // }
-
-            // point.y += 5;
-            // if (checkAllCollisions(point)) {
-            //     console.log('here');
-            //     return true;
-            // }
-
-            // point.x - 5
-            // if (checkAllCollisions(point)) {
-            //     console.log('here');
-            //     return true;
-            // }
-
         }
         return false;
     }
@@ -143,7 +118,7 @@ window.onload = function () {
 
 
     // Creates red circles based on furthest blue squares from circle
-    function getRedCircles() {
+    function createRedCircles() {
         // ctx.beginPath();
         // ctx.strokeStyle = "red";
         // ctx.arc(center.x, center.y, largeRedRadius, 0, 2 * Math.PI);
@@ -154,32 +129,12 @@ window.onload = function () {
 
 
 
-
-
-
     function checkCollision(rect1, rect2) {
-
         if (rect1.x < rect2.x + rect2.width &&
             rect1.x + rect1.width > rect2.x &&
             rect1.y < rect2.y + rect2.height &&
             rect1.y + rect1.height > rect2.y) {
             return true;
-        }
-    }
-
-
-
-
-
-
-
-
-    //Check each block in maze for collision
-    function checkAllCollisions(p) {
-        for (let i = 0; i < squares.length; i++) {
-            if (checkCollision(p, squares[i+1])) {
-                return true;
-            }
         }
     }
 
